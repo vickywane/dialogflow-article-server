4f3c48bd916483a6c76dbff28a645f8816dea8a4
victory <vickywane@gmail.com>
2020-11-24 17:35:16 +0100

    Final update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 exports.foodFunction = async (req, res) => {
   const { MongoClient } = require("mongodb");
 
-  const { food } = req.body.queryResult.parameters;
+  // const { food } = req.body.queryResult.parameters;
   const CONNECTION_URI = process.env.MONGODB_URI;
   // initate a connection to the deployed mongodb cluster
 
@@ -19,38 +19,87 @@ exports.foodFunction = async (req, res) => {
     }
     const collection = client.db(process.env.DATABASE_NAME).collection("Meals");
 
-   
+    const { displayName } = req.body.queryResult.intent;
 
-    // res.status(200).json(response);
-
-    const data = collection.find({ name: food });
     const result = [];
+    switch (displayName) {
+      case "list-available-meals":
+        const data = collection.find({});
+        const meals = [
+          {
+            text: {
+              text: [
+                `We currently have the following 20 meals on our menu list. Which would you like to request for?`,
+              ],
+            },
+          },
+        ];
 
-    result.push(
-      data.forEach((item) => {
-        result.push(item);
-      })
-    );
+        result.push(
+          data.forEach((item) => {
+            const { name, description, price, image_uri } = item;
 
-    Promise.all(result)
-      .then((_) => {
-        const { name, availableUnits, price, description } = result[1];
-        // const responseText =
+            const card = {
+              card: {
+                title: `${name} at $${price}`,
+                subtitle: description,
+                imageUri: image_uri,
+              },
+            };
 
-        const response = {
-          fulfillmentText: "This is a text response",
-          fulfillmentMessages: [
+            meals.push(card);
+          })
+        );
+
+        return Promise.all(result)
+          .then((_) => {
+            const response = {
+              fulfillmentMessages: meals,
+            };
+
+            res.status(200).json(response);
+          })
+          .catch((e) => res.status(400).send({ error: e }));
+
+      case "request-meal":
+        const { food } = req.body.queryResult.parameters;
+        collection.findOne({ name: food }, (err, data) => {
+          if (err) {
+            res.status(400).send({ error: err });
+          }
+
+          const { name, price, description, image_uri } = data;
+
+          const singleCard = [
             {
               text: {
-                text: [`${name} are available in ${availableUnits} each at ${price}.`],
+                text: [`The ${name} is currently priced at $${price}.`],
               },
             },
-          ],
-        };
 
-        res.status(200).json(response);
-      })
-      .catch((e) => res.status(400).send({ error: e }));
+            {
+              card: {
+                title: `${name} at $${price}`,
+                subtitle: description,
+                imageUri: image_uri,
+                buttons: [
+                  {
+                    text: "Pay For Meal",
+                    postback: "htts://google.com",
+                  },
+                ],
+              },
+            },
+          ];
+
+          res.status(200).json({ fulfillmentMessages: singleCard });
+        });
+
+      default:
+        break;
+    }
+
+    // const result = [];
 
     client.close();
   });
